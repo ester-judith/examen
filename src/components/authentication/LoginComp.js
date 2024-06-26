46a759662d9bcdd4a00a05a0616d67ad2d706565
ester-judith <ester.j.ejcc@gmail.com>
2024-06-26 09:38:25 -0700

    septimo commit

--- src/components/authentication/LoginComp.js
+++ src/components/authentication/LoginComp.js
@@ -1,9 +1,52 @@
-import React from "react";
+import { Modal, Form, Button, Alert } from "react-bootstrap";
+import React, { useState, useRef } from "react";
 
 export const LoginComp = () => {
+    const [showForm, setShowForm] = useState(false);
+    const [error, setError] = useState('');
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const openForm = () => setShowForm(true);
+    const closeForm = () => setShowForm(false);
+    const submitForm = (e) => {
+        e.preventDefault();
+        setError('');
+
+        console.log(`email is ${emailRef.current.value}`);
+        console.log(`passwordRef is ${passwordRef.current.value}`);
+    };
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <div onClick={openForm} className="btn btn-outline-secondary mx-2">Iniciar sesion</div>
+            <Modal centered show={showForm} onHide={closeForm}>
+                <form onSubmit={submitForm}>
+                    <Modal.Header>
+                        <Modal.Title>Iniciar sesion</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form.Group>
+                            <Form.Label>Correo electronico</Form.Label>
+                            <Form.Control type="email" required ref={emailRef}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" required ref={passwordRef}/>
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeForm}>
+                            Cancelar
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Iniciar sesion
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
+        </>
     )
 }
--- src/components/authentication/RegisterComp.js
+++ src/components/authentication/RegisterComp.js
@@ -1,42 +1,58 @@
-import { Modal, Form, Button } from "react-bootstrap";
-import React, { useState } from "react";
+import { Modal, Form, Button, Alert } from "react-bootstrap";
+import React, { useState, useRef } from "react";
 
 export const RegisterComp = () => {
     const [showForm, setShowForm] = useState(false);
+    const [error, setError] = useState('');
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const cmfPasswordRef = useRef();
+
     const openForm = () => setShowForm(true);
     const closeForm = () => setShowForm(false);
     const submitForm = (e) => {
         e.preventDefault();
+        setError('');
+
+        if (passwordRef.current.value !== cmfPasswordRef.current.value) {
+            return setError("Contraseña no concuerda")
+        }
+
+        console.log(`email is ${emailRef.current.value}`);
+        console.log(`passwordRef is ${passwordRef.current.value}`);
+        console.log(`cmfPasswordRef is ${cmfPasswordRef.current.value}`);
     };
 
     return (
         <>
-            <div onClick={openForm} className="btn btn-outline-secondary mx-2">Register</div>
+            <div onClick={openForm} className="btn btn-outline-secondary mx-2">Registro</div>
             <Modal centered show={showForm} onHide={closeForm}>
                 <form onSubmit={submitForm}>
                     <Modal.Header>
-                        <Modal.Title>Register</Modal.Title>
+                        <Modal.Title>Registro</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        {error && <Alert variant="danger">{error}</Alert>}
                         <Form.Group>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" required/>
+                            <Form.Label>Correo electronico</Form.Label>
+                            <Form.Control type="email" required ref={emailRef}/>
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" required/>
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" required ref={passwordRef}/>
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Confirm password</Form.Label>
-                            <Form.Control type="password" required/>
+                            <Form.Label>Confirmar contraseña</Form.Label>
+                            <Form.Control type="password" required ref={cmfPasswordRef}/>
                         </Form.Group>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={closeForm}>
-                            Cancel
+                            Cancelar
                         </Button>
-                        <Button variant="seconday" type="submit">
-                            Register
+                        <Button variant="primary" type="submit">
+                            Registrar
                         </Button>
                     </Modal.Footer>
                 </form>
