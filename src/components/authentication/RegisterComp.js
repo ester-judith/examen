8906c51ecf10c64e5d02574bcf51ad16622f906e
alessosos <bradburylilip@gmail.com>
2024-06-26 19:43:20 -0700

    shari cambios diseño

--- src/components/authentication/RegisterComp.js
+++ src/components/authentication/RegisterComp.js
@@ -9,7 +9,7 @@ export const RegisterComp = () => {
     const emailRef = useRef();
     const passwordRef = useRef();
     const cmfPasswordRef = useRef();
-    const { register } = useContext(AuthContext)
+    const { register } = useContext(AuthContext);
 
     const openForm = () => setShowForm(true);
     const closeForm = () => setShowForm(false);
@@ -18,50 +18,55 @@ export const RegisterComp = () => {
         setError('');
 
         if (passwordRef.current.value !== cmfPasswordRef.current.value) {
-            return setError("Contraseña no concuerda")
+            return setError("La contraseña no coincide");
         }
 
         try {
             await register(emailRef.current.value, passwordRef.current.value);
             closeForm();
         } catch (error) {
-            setError(error.message)
+            setError(error.message);
         }
     };
 
     return (
         <>
-            <div onClick={openForm} className="btn btn-outline-secondary mx-2">Registro</div>
+            <div onClick={openForm} className="btn btn-outline-danger mx-2">
+                Registrarse
+            </div>
             <Modal centered show={showForm} onHide={closeForm}>
                 <form onSubmit={submitForm}>
-                    <Modal.Header>
-                        <Modal.Title>Registro</Modal.Title>
+                    <Modal.Header style={{ backgroundColor: "#d69496", border: "1px solid #dddddd" }}>
+                        <Modal.Title style={{ color: "#ffffff" }}>
+                            Registrarse
+                        </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         {error && <Alert variant="danger">{error}</Alert>}
                         <Form.Group>
-                            <Form.Label>Correo electronico</Form.Label>
-                            <Form.Control type="email" required ref={emailRef}/>
+                            <Form.Label style={{ color: "#d69496", fontSize: "1.0em", fontWeight: "bold" }}>Correo Electrónico</Form.Label>
+                            <Form.Control type="email" required ref={emailRef} />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control type="password" required ref={passwordRef}/>
+                            <Form.Label style={{ color: "#d69496", fontSize: "1.0em", fontWeight: "bold" }}>Contraseña</Form.Label>
+                            <Form.Control type="password" required ref={passwordRef} />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Confirmar contraseña</Form.Label>
-                            <Form.Control type="password" required ref={cmfPasswordRef}/>
+                            <Form.Label style={{ color: "#d69496", fontSize: "1.0em", fontWeight: "bold" }}>Confirmar Contraseña</Form.Label>
+                            <Form.Control type="password" required ref={cmfPasswordRef} />
                         </Form.Group>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={closeForm}>
+                        <Button variant="secondary" onClick={closeForm} style={{ backgroundColor: '#ddd', color: '#000' }}>
                             Cancelar
                         </Button>
-                        <Button variant="primary" type="submit">
-                            Registrar
+                        <Button variant="danger" type="submit" style={{ backgroundColor: '#f5b2c2', color: '#000' }}>
+                            Registrarse
                         </Button>
                     </Modal.Footer>
                 </form>
             </Modal>
         </>
-    )
-}
+    );
+};
+  
